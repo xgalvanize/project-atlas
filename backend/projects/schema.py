96cfcc8af750bb102712ac0001c0626f77132c03
Brian Cameron <bjc008@gmail.com>
2026-01-21 23:16:27 -0500

    Expose Action in projects GraphQL schema

--- backend/projects/schema.py
+++ backend/projects/schema.py
@@ -1,13 +1,20 @@
 import graphene
 from graphene_django import DjangoObjectType
 from django.contrib.auth.models import User
-from .models import Project
+from .models import Project, Action
+
+
+class ActionType(DjangoObjectType):
+    class Meta:
+        model = Action
+        fields = "__all__"
 
 
 class UserType(DjangoObjectType):
     class Meta:
         model = User
         fields = ("id", "username")
+
 
 class ProjectType(DjangoObjectType):
     owner = graphene.Field(UserType)
@@ -18,7 +25,14 @@ class ProjectType(DjangoObjectType):
 
 
 class ProjectsQuery(graphene.ObjectType):
+    # Projects
     projects = graphene.List(ProjectType)
 
-    def resolve_projects(root, info):
+    def resolve_projects(self, info):
         return Project.objects.all()
+
+    # Actions
+    actions = graphene.List(ActionType)
+
+    def resolve_actions(self, info):
+        return Action.objects.all()
