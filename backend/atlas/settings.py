920c8e4594cb512634e13d516398bdb0b70676c9
Brian Cameron <bjc008@gmail.com>
2026-02-02 21:10:46 -0500

    broken

--- backend/atlas/settings.py
+++ backend/atlas/settings.py
@@ -81,30 +81,16 @@ WSGI_APPLICATION = 'atlas.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'atlas_db',
-#         'USER': 'atlas_user',
-#         'PASSWORD': 'strongpassword',
-#         'HOST': 'localhost',  # change to k3s service later
-#         'PORT': '5432',
-#     }
-# }
+import os
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "atlas_db",
-        "USER": "atlas_user",
-        "PASSWORD": "strongpassword",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+        "NAME": os.environ.get("DB_NAME", "atlas_db"),
+        "USER": os.environ.get("DB_USER", "atlas_user"),
+        "PASSWORD": os.environ.get("DB_PASSWORD", "strongpassword"),
+        "HOST": os.environ.get("DB_HOST", "127.0.0.1"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
     }
 }
 
