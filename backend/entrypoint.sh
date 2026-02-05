b9719530397d8c57b2e5ce328e483f9936ca05bd
Brian Cameron <bjc008@gmail.com>
2026-02-05 16:35:59 -0500

    broken

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,6 +4,30 @@ set -e
 # Run migrations
 python manage.py migrate --noinput
 
+# Create Django superuser if ADMIN_USERNAME and ADMIN_PASSWORD are provided
+if [ -n "$ADMIN_USERNAME" ] && [ -n "$ADMIN_PASSWORD" ]; then
+	echo "Checking/creating superuser $ADMIN_USERNAME"
+	python - <<'PY'
+import os
+import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'atlas.settings')
+django.setup()
+from django.contrib.auth import get_user_model
+User = get_user_model()
+username = os.environ.get('ADMIN_USERNAME')
+email = os.environ.get('ADMIN_EMAIL', '')
+password = os.environ.get('ADMIN_PASSWORD')
+if username and password:
+		if not User.objects.filter(username=username).exists():
+				User.objects.create_superuser(username, email, password)
+				print('Created superuser', username)
+		else:
+				print('Superuser exists', username)
+PY
+else
+	echo "ADMIN_USERNAME or ADMIN_PASSWORD not set; skipping superuser creation"
+fi
+
 # Collect static files
 python manage.py collectstatic --noinput
 
